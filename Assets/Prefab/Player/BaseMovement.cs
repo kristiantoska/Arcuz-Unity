73934e88ca8830aaac8d63b53c6005137573b5a9
kristiantoska <kristiantoska1@gmail.com>
2023-07-04 20:24:03 +0200

    add attack colliders

--- Assets/Prefab/Player/BaseMovement.cs
+++ Assets/Prefab/Player/BaseMovement.cs
@@ -12,12 +12,25 @@ public class BaseMovement : MonoBehaviour {
     public Animator animator;
 
     public float moveSpeed = 5f;
-    public float horizontal;
-    public float vertical;
 
+    private float horizontal;
+    private float vertical;
     private bool isFacingRight = true;
 
+    private Transform attackingPoint;
+    public Transform attackPointDown;
+    public Transform attackPointUp;
+    public Transform attackPointRight;
+    public Transform attackPointLeft;
+
+    public LayerMask enemyLayers;
+    public float attackRange;
+    private bool attacking = false;
+
     void Update () {
+        if (attacking) {
+            return;
+        }
         horizontal = Input.GetAxisRaw ("Horizontal");
         vertical = Input.GetAxisRaw ("Vertical");
 
@@ -39,14 +52,50 @@ public class BaseMovement : MonoBehaviour {
         }
 
         if (Input.GetKeyDown (KeyCode.J)) {
-            animator.SetTrigger ("SwordAttack");
+            Attack ();
         }
     }
 
     void FixedUpdate () {
+        if (attacking) {
+            return;
+        }
         Vector2 moveVector = new Vector2 (horizontal, vertical);
         moveVector = Vector2.ClampMagnitude (moveVector, 1f);
 
         rb.MovePosition (rb.position + moveVector * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Attack () {
+        animator.SetTrigger ("SwordAttack");
+        attacking = true;
+        Invoke ("AttackEnd", 0.5f);
+
+        float lastDirection = animator.GetFloat ("LastDirection");
+        if (lastDirection == TopDirection) {
+            attackingPoint = attackPointUp;
+        } else if (lastDirection == BottomDirection) {
+            attackingPoint = attackPointDown;
+        } else if (isFacingRight) {
+            attackingPoint = attackPointRight;
+        } else {
+            attackingPoint = attackPointLeft;
+        }
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll (attackingPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies) {
+            Debug.Log ("Hit enemy" + enemy.name);
+        }
+    }
+
+    void AttackEnd () {
+        attacking = false;
+    }
+
+    void OnDrawGizmos () {
+        if (attackingPoint == null || !attacking) {
+            return;
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere (attackingPoint.position, attackRange);
     }
 }
